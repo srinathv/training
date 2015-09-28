929e695aae9165c801372382bf46ff1162242701
Srinath Vadlamani <srinath@txcorp.com>
2015-09-28 16:13:54 -0700

    addign my own version of buil on Babbage

--- hackathon-201502/BGW/buildRun.tau.sh
+++ hackathon-201502/BGW/buildRun.tau.sh
@@ -1,3 +1,4 @@
 export OMP_NUM_THREADS=16
 export TAU_MAKEFILE=/project/projectdirs/acts/vadlaman/tau/2.24.1/intel/16/x86_64/lib/Makefile.tau-icpc-papi-pdt-openmp-opari
 tau_f90.sh -optKeepFiles -O3 -xAVX bgw.f90 -o bgw.tau
+./bgw.tau
